c2dd302affe9ac8ef15a7297b187d73086388253
ajkfds <ajkfds2018@gmail.com>
2025-12-17 07:33:09 +0900

    fix parse system

--- CodeEditor2MarkdownPlugin/CodeEditor2MarkdownPlugin/NavigatePanel/MarkdownFileNode.cs
+++ CodeEditor2MarkdownPlugin/CodeEditor2MarkdownPlugin/NavigatePanel/MarkdownFileNode.cs
@@ -44,7 +44,7 @@ namespace pluginMarkdown.NavigatePanel
 
             UpdateVisual();
 
-            if (TextFile.ParseValid && !TextFile.ReparseRequested)
+            if (!TextFile.ReparseRequested)
             {
                 // skip parse
             }
--- CodeEditor2MarkdownPlugin/CodeEditor2MarkdownPlugin/Parser/Parser.cs
+++ CodeEditor2MarkdownPlugin/CodeEditor2MarkdownPlugin/Parser/Parser.cs
@@ -22,7 +22,7 @@ namespace pluginMarkdown.Parser
             ParsedDocument = new CodeEditor2.CodeEditor.ParsedDocument(file,file.RelativePath, file.CodeDocument.Version, parseMode);
         }
 
-        public override async Task Parse()
+        public override async Task ParseAsync()
         {
             for(int line = 1; line<Document.Lines; line++)
             {
